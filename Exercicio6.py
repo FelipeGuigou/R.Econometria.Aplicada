5e2c983ac046a4a3f83621788c39aea6e660a8be
FelipeGuigou <39139103+FelipeGuigou@users.noreply.github.com>
2018-05-19 16:26:43 -0300

    Update Exercicio6.py

--- Exercicio6.py
+++ Exercicio6.py
@@ -2,9 +2,10 @@
 a = float(input("Digite sua velocidade em quilômetros por hora:"))
 
 #Processamento
-if a > 120:
-	b = (a-120)*5
+b = (a-120)*5
+
 #Saída
+if a > 120:
 	print("Você levou uma multa por excesso de velocidade, sua multa tem valor (em reais) de:", b)	
 else: 
 	print("Você não levou uma multa por excesso de velocidade, mantenha sua prudência no trânsito!")
