c858a8b2553ed754337a823621fd9850999b9824
FelipeGuigou <39139103+FelipeGuigou@users.noreply.github.com>
2018-05-28 18:47:47 -0300

    Add files via upload

---
+++ Aula25.05/EX12/EX12.py
@@ -0,0 +1,26 @@
+'''
+
+Exercício 12: Apresenta a tabuada.
+Autores: Felipe Guigou e Cirillo Stassen
+Versão: 0.1
+Comentário: Referente a aula de 25/05/2018.
+
+'''
+
+#Entrada de Dados
+
+i = 1
+
+#Processamento de Dados
+
+
+
+#Saída
+
+while i <= 9:
+	j = 1
+	while j <= 9:
+		print(i, "x", j, "=", i*j)
+		j+=1
+	i+=1
+
