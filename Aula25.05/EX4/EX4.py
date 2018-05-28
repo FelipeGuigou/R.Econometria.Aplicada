8aa17e0f2804eacb4ae516c0ecd9de5be0deaead
FelipeGuigou <39139103+FelipeGuigou@users.noreply.github.com>
2018-05-28 18:42:36 -0300

    Add files via upload

---
+++ Aula25.05/EX4/EX4.py
@@ -0,0 +1,21 @@
+'''
+
+Exercício 4: Dobra e apresenta o Consumo Agregado digitado
+Autores: Felipe Guigou e Cirillo Stassen
+Versão: 0.1
+Comentário: Referente a aula de 25/05/2018.
+
+'''
+
+#Entrada de Dados
+
+ConsumoAgregado = float(input("Digite o valor do COnsumo Agregado:"))
+
+#Processamento de Dados
+
+dobroconsumo = 2*ConsumoAgregado
+
+#Saída
+
+print("O dobro do Consumo Agregado totaliza:", dobroconsumo)
+
