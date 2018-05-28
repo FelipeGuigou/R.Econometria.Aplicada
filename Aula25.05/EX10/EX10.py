1f4248a6546ef22694f851e90d6ce8a9e3f8a3ed
FelipeGuigou <39139103+FelipeGuigou@users.noreply.github.com>
2018-05-28 18:46:22 -0300

    Add files via upload

---
+++ Aula25.05/EX10/EX10.py
@@ -0,0 +1,23 @@
+'''
+
+Exercício 10: Apresenta números sequencialmente.
+Autores: Felipe Guigou e Cirillo Stassen
+Versão: 0.1
+Comentário: Referente a aula de 25/05/2018.
+
+'''
+
+#Entrada de Dados
+
+i = 1
+
+#Processamento de Dados
+
+
+
+#Saída
+
+while i <= 3:
+	print(i)
+	i = i+1
+
