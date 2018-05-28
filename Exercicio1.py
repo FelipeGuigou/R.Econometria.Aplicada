f11a3d66b2cdebd27b21c9e9a08258bff826f95e
FelipeGuigou <39139103+FelipeGuigou@users.noreply.github.com>
2018-05-28 18:26:07 -0300

    Delete Exercicio1.py

--- Exercicio1.py
+++
@@ -1,9 +0,0 @@
-#Entrada de Dados
-a = int(input("Digite o primeiro número:"))
-b = int(input("Digite o segundo número:"))
-
-#Processamento de Dados
-soma = a+b
-
-#Saída
-print("O resultado é:", soma)
