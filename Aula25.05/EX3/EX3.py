501b724159157b3cc9ccc07cc93b9fb9bf7ee02c
FelipeGuigou <39139103+FelipeGuigou@users.noreply.github.com>
2018-05-28 18:41:58 -0300

    Add files via upload

---
+++ Aula25.05/EX3/EX3.py
@@ -0,0 +1,20 @@
+'''
+
+Exercício 3: Atribuindo Valor a uma variável via um arquivo .dat
+Autores: Felipe Guigou e Cirillo Stassen
+Versão: 0.1
+Comentário: Referente a aula de 25/05/2018.
+
+'''
+
+#Entrada de Dados
+
+ConsumoAgregado = 100000
+
+#Processamento de Dados
+
+
+
+#Saída
+
+
