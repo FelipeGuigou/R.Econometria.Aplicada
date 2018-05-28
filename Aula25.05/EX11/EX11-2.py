7c7a43284ac7930a09bc012c486ad9413a31ec93
FelipeGuigou <39139103+FelipeGuigou@users.noreply.github.com>
2018-05-28 18:47:16 -0300

    Add files via upload

---
+++ Aula25.05/EX11/EX11-2.py
@@ -0,0 +1,23 @@
+'''
+
+Exercício 11-2: Apresenta algarismos de forma sequencial até limite informado.
+Autores: Felipe Guigou e Cirillo Stassen
+Versão: 0.1
+Comentário: Referente a aula de 25/05/2018.
+
+'''
+
+#Entrada de Dados
+
+i = 1
+
+#Processamento de Dados
+
+j = float(input("Digite o número de iterações a serem cumpridas:"))
+
+#Saída
+
+while i <= j:
+	print(i)
+	i +=1
+
---
+++ Aula25.05/EX11/EX11.py
@@ -0,0 +1,23 @@
+'''
+
+Exercício 11: Apresenta algarismos de forma sequencial.
+Autores: Felipe Guigou e Cirillo Stassen
+Versão: 0.1
+Comentário: Referente a aula de 25/05/2018.
+
+'''
+
+#Entrada de Dados
+
+i = 1
+
+#Processamento de Dados
+
+
+
+#Saída
+
+while i <= 100:
+	print(i)
+	i +=1
+
