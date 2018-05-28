8b34eaf140cf93e9bb5ebb906e3a4b93d74a8b3e
FelipeGuigou <39139103+FelipeGuigou@users.noreply.github.com>
2018-05-28 18:43:56 -0300

    Add files via upload

---
+++ Aula25.05/EX6/EX6.py
@@ -0,0 +1,21 @@
+'''
+
+Exercício 6: Toma uma temperatura em Celsius e a converte para Fahrenheit.
+Autores: Felipe Guigou e Cirillo Stassen
+Versão: 0.1
+Comentário: Referente a aula de 25/05/2018.
+
+'''
+
+#Entrada de Dados
+
+Celsius = float(input("Digite uma temperatura em Celsius:"))
+
+#Processamento de Dados
+
+Fahrenheit = (Celsius*9/5) + 32
+
+#Saída
+
+print("A temperatura em graus Fahrenheit é:", Fahrenheit)
+
